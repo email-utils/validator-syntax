c37a4a3a3cddaad8f4af1d23298f5100be56a597
Simeon Smith <simeon.smith@14four.com>
2021-12-01 14:06:56 -0800

    finished tag command

--- bin/tag.sh
+++ bin/tag.sh
@@ -4,13 +4,13 @@ if [ $1 ]; then
   echo "Creating a new $1 version."
 
   npm version $1
-  version=`git diff HEAD^..HEAD -- "$(git rev-parse --show-toplevel)"/package.json | grep '^\+.*version' | sed -s 's/[^0-9\.]//g'`
+  version=`node -e "console.log(require('./package.json').version);"`
 
   npm run build
   git add -A
   git commit -m "Building for $version."
 
-  echo "Creating version $version."
+  echo "Publishing version $version."
 
   git push
   git push --tags
