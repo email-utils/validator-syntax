83bf6a0662de84d80ce9e69d8f940b0245a1a7d8
Simeon Smith <simeon.smith@14four.com>
2021-12-01 14:04:37 -0800

    Building for .

--- bin/tag.sh
+++ bin/tag.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-echo "Creating a $1 verion."
 
-npm version $1
-version=`git diff HEAD^..HEAD -- "$(git rev-parse --show-toplevel)"/package.json | grep '^\+.*version' | sed -s 's/[^0-9\.]//g'`
+if [ $1 ]; then
+  echo "Creating a new $1 version."
 
-echo "Creating version $version."
+  npm version $1
+  version=`git diff HEAD^..HEAD -- "$(git rev-parse --show-toplevel)"/package.json | grep '^\+.*version' | sed -s 's/[^0-9\.]//g'`
 
-git push
-git push --tags
-npm publish --access public
+  npm run build
+  git add -A
+  git commit -m "Building for $version."
+
+  echo "Creating version $version."
+
+  git push
+  git push --tags
+  npm publish --access public
+else
+  echo "No version type provided. (ie. ./bin/tag.sh version)";
+fi
+
