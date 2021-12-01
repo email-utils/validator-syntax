64980cfb0f9f81dbb869dfcb79691b29d33f49df
Simeon Smith <simeon.smith@14four.com>
2021-12-01 13:38:55 -0800

    Setup tagging.

---
+++ bin/tag.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Creating a $1 verion."
+
+npm version $1
+version=`git diff HEAD^..HEAD -- "$(git rev-parse --show-toplevel)"/package.json | grep '^\+.*version' | sed -s 's/[^0-9\.]//g'`
+git add *;
+git commit -m "Updating tag $version"
+
+
+if [ "$version" != "" ]; then
+    git tag -a "v$version" -m "`git log -1 --format=%s`"
+    echo "Created a new tag, v$version"
+    git push --tags
+    npm publish
+fi
